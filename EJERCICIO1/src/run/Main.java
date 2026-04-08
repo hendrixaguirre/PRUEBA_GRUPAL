1d34c0b72a862f2ef5b3a313cf899ba96dd707bc
poqta <pcosquist@uamv.edu.ni>
2026-04-07 20:46:04 -0600

    Ultimas correciones

--- EJERCICIO1/src/run/Main.java
+++ EJERCICIO1/src/run/Main.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         ArrayList<Estudiante> lista = new ArrayList<>();
