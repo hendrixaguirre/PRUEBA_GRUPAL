907a17b7c6aad802b7c2eacc85516e2389ba4aa7
chenaoh <jjmontealto@uamv.edu.ni>
2026-04-07 20:07:05 -0600

    Clase Estudiante con atributos, getters/setters y toString

---
+++ EJERCICIO1/src/models/Estudiante.java
@@ -0,0 +1,60 @@
+package models;
+
+public class Estudiante {
+    private String codigo;
+    private String nombre;
+    private float promedio;
+    private Carrera carrera;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String codigo, String nombre, float promedio, Carrera carrera) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.promedio = promedio;
+        this.carrera = carrera;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", promedio=" + promedio +
+                ", carrera=" + carrera +
+                '}';
+    }
+}
